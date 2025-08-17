0a0e5b5fd8739e47640099353ab7263d4bc5cbb1
Mistel Geehrt <mistelgeehrt@gmail.com>
2025-08-17 02:13:42 +0100

    Add session, flash and CSRF protection

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+const csrf = require('csurf');
+const { ensureAuth } = require('./middleware/auth');
 const ussdCodes = require('./config/ussd.json');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+app.use(csrf());
+
+app.use((req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.get('/auction/new', ensureAuth, (req, res) => {
+  res.send('New auction form');
+});
+
+app.post('/auction/:id/bid', ensureAuth, (req, res) => {
+  res.send(`Bid placed on auction ${req.params.id}`);
+});
+
+app.post('/auction/:id/buy', ensureAuth, (req, res) => {
+  res.send(`Buy now auction ${req.params.id}`);
+});
+
+app.get('/profile', ensureAuth, (req, res) => {
+  res.send('User profile');
+});
 
 const auctions = {
   '1': { amount: 1000, ref: 'AUC1' },
   '2': { amount: 2000, ref: 'AUC2' }
 };
 
-app.get('/payment/:auctionId', (req, res) => {
+app.get('/payment/:auctionId', ensureAuth, (req, res) => {
   const auction = auctions[req.params.auctionId];
   if (!auction) {
     return res.status(404).send('Auction not found');
@@ -40,3 +75,4 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
